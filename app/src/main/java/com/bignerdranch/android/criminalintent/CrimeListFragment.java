9128dea92da667de7988f53dd48c7d7cf5a22e73
Viacheslav <demon55591@yandex.ru>
2019-02-02 12:32:58 +0300

    chapter 8 (added fragment for list of crimes)

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
@@ -0,0 +1,10 @@
+package com.bignerdranch.android.criminalintent;
+
+import android.annotation.SuppressLint;
+import android.support.v4.app.Fragment;
+
+@SuppressLint("ValidFragment")
+class CrimeListFragment extends Fragment {
+
+
+}
